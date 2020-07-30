0171e49f2a68fe1a4ed85b3486d1eb27834b20cd
anshika451 <anshikamishra451@gmail.com>
2020-07-30 13:20:55 -0400

    authentication and security

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require("morgan");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -8,16 +14,53 @@ const userRouter = require('./routes/userRouter');
 
 const app = express();
 
-// 1) MIDDLEWARES
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+app.use(express.json({limit: '10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+      whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+      ]
+    })
+);
+
+//serving stastic files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next)=>{
     req.requestTime = new Date().toISOString();
+    console.log(req.headers);
     next();
 });
 
@@ -30,7 +73,7 @@ app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
   });
   
-  app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
 module.exports = app;
 
